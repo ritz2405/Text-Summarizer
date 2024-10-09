3bc41972924d55431ec9f4e1f67cca887b15183c
ritz2405 <ruturajpatil782@gmail.com>
2024-10-10 01:28:26 +0530

    text summarizer

---
+++ text_summarizer.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import torch
+import re
+
+from transformers import pipeline
+
+summarizer = pipeline('summarization', model='facebook/bart-large-cnn')
+
+st.write("""
+# Text Summarizer
+""")
+
+text_input = st.text_area("Text to summarize")
+
+if text_input:
+    text_input = str(text_input)
+
+st.write("## Summarized Text")
+output_text = str(summarizer(text_input, max_length=130, min_length=30, do_sample=False))
+output_text = re.sub(r'[^\w\s]', '', output_text)
+output_text = output_text.split(' ', 1)[1]
+output_text = str(output_text)
+st.write("".join(output_text))
